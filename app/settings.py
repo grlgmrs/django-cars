e54315572fcb3a9d8a1a7e502298d3f28458c23c
Gabriel Guimarães <gabriel@recruitrobin.com>
2025-03-21 10:02:30 -0300

    :construction: Create app 'cars'

--- app/settings.py
+++ app/settings.py
@@ -30,6 +30,10 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+CUSTOM_APPS = [
+    'cars'
+]
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -37,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    *CUSTOM_APPS
 ]
 
 MIDDLEWARE = [
